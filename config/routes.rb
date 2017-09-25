1521b2435a5d8745dcd665ccc7b7781d2dc7a129
hussein0said@gmail.com <hussein0said@gmail.com>
2017-09-25 15:54:48 +0200

    create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+ 
+#root to: 'static_pages#landing'
   get 'test/index'
 
   get 'demo/index'
   get 'demo/hello'
+  root to: 'home#index'
+
+#root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
